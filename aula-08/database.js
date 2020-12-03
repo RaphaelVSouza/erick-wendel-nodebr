343b81ab8c83a9d2a68848d1a6d3540f8152f832
Raphael Vieira <raphael5254@outlook.com>
2020-12-03 14:54:04 -0300

    Aula-08 CRUD - Create

--- aula-08/database.js
+++ aula-08/database.js
@@ -10,10 +10,30 @@ class Database {
 
   async readFileAsync() {
     const file = await readFile(this.NOME_ARQUIVO, 'utf-8');
+
     return JSON.parse(file.toString());
   }
 
-  async writeFileAsync() {
+  async writeFileAsync(data) {
+    const file = this.NOME_ARQUIVO;
+    await writeFile(file, JSON.stringify(data));
+    return;
+  }
+
+  async create(hero) {
+    const data = await this.readFileAsync();
+    const id = hero.id <= 2 ? hero.id : Date.now();
+    const heroWithId = {
+      id,
+      ...hero
+    };
+
+    const finalData = [...data, heroWithId];
+
+    const result = await this.writeFileAsync(finalData);
+
+    return result;
+
 
   }
 
--- aula-08/test.js
+++ aula-08/test.js
@@ -2,12 +2,25 @@ const { deepStrictEqual, ok } = require('assert');
 
 const database = require('./database');
 
-const DEFAULT_INDEX_ITEM = {id: 1, nome: 'Flash', poder: 'Speed'};
+const DEFAULT_ITEM = {id: 1, nome: 'Flash', poder: 'Speed'};
+const DEFAULT_NEW_ITEM = {id: 2, nome: 'Batman', poder: 'Money'};
 
 describe('Suite de manipulação de Herois', () => {
+  before(async () => {
+    await database.create(DEFAULT_NEW_ITEM)
+  });
+
   it('deve pesquisar um herói usando arquivos', async() => {
-    const expected = DEFAULT_INDEX_ITEM;
+    const expected = DEFAULT_ITEM;
     const [ result ] = await database.index(expected.id);
     deepStrictEqual(result, expected);
+  })
+
+
+  it('deve cadastrar um herói usando arquivos', async() => {
+    const expected = DEFAULT_NEW_ITEM;
+    const result = await database.create(DEFAULT_NEW_ITEM);
+    const [ actual ] = await database.index(expected.id)
+    deepStrictEqual(actual, expected);
   })
 })
